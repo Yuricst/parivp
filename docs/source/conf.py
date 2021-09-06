d7e40c5033c3fe319220620aad6cb2639db7bb7b
Yuri <yuri.shimane@gmail.com>
2021-09-05 20:13:46 -0400

    Updates requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,3 +52,15 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+
+########### TRICK FOUND ON SOME TUTORIAL : ADD IN THE MOCK_MODULES ANY EXTERNAL MODULE YOU'RE USING IN YOUR PACKAGE.
+
+import mock
+
+MOCK_MODULES = ['numpy', 'matplotlib', 'scipy']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
+
